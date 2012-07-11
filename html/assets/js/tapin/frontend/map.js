d7c1e164d91d055d13bf838d6443e953830bdc94
Tyler Menezes <tylermenezes@gmail.com>
2012-07-11 16:33:20 -0700

    Revert "Undo Vu's bad revert"
    This reverts commit 4afb7cd635eaa5ec8289e0930cfa689c00a0c50f.

--- html/assets/js/tapin/frontend/map.js
+++ html/assets/js/tapin/frontend/map.js
@@ -237,11 +237,8 @@ define(['tapin/frontend/map/pincollection', 'tapin/util/log', 'tapin/util/event'
 
             $.getScript('http://j.maxmind.com/app/geoip.js', function()
             {
-                var lat = geoip_latitude();
-                var lon = geoip_longitude();
-                Log('info', 'GeoIP detected as ' + lat + ', ' + lon);
-                _this.center(lat, lon);
-
+                _this.center(geoip_latitude(), geoip_longitude());
+                _this.zoom(9);
             });
 
             $(document).mouseup(function(){
