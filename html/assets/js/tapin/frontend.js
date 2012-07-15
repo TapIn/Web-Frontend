bad4a3188f85f6c0e6f643c40badd685e723efbb
Vu Tran <vu@Vus-MacBook-Pro.local>
2012-07-14 19:19:43 -0700

    add comment scroll

--- html/assets/js/tapin/frontend.js
+++ html/assets/js/tapin/frontend.js
@@ -235,7 +235,7 @@ define([
             });
 
             $('#comment-form').click(function(){
-                document.getElementById('comment-form').style.height = '60px';
+                $('#comment-form').height(60);
             });
 
             //Handles what happens when you click 'lastweek' button
@@ -297,6 +297,14 @@ define([
 
             $('a#stream').click(function(){
                 alert();
+            });
+
+            $('#more').click(function(){
+                var comment = $("div id='comments'><div class='comment'><div class='user-icon'></div><div class='body'>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non!<div class='time'>about 20 minutes ago</div></div></div>");
+                var comment2 = $("div id='comments'><div class='comment'><div class='user-icon'></div><div class='body'>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non!<div class='time'>about 20 minutes ago</div></div></div>");
+
+                $('#comments').append(comment);
+                $('#comments').append(comment2);
             });
            
             // Bind to volume change events
