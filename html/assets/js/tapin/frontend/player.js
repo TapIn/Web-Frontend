5fd9bd50f9c46f4ab3d678d54fed91136ef01a81
Tyler Menezes <tylermenezes@gmail.com>
2012-07-22 22:27:06 +0000

    Bug fixes for video player Fix progress bar glitch - Fix #126 Fix play button glitch - Fix #127 Fix volume bar - Fix #121 Add error image for broken streams - Fix #134

--- html/assets/js/tapin/frontend/player.js
+++ html/assets/js/tapin/frontend/player.js
@@ -114,6 +114,8 @@ define(['flowplayer', 'tapin/util/log', 'jquery'], function(Flowplayer, Log, JQu
                     Log('error', 'Could not play ' + (clip.isLive? 'live' : 'recorded') + ' stream', clip);
                     if (clip.isLive) {
                         _this.playRecordedLive(clip.streamId);
+                    } else {
+                        player_div.html('<img src="assets/img/viderror.png" />')
                     }
                 },
                 onEnded: null,
