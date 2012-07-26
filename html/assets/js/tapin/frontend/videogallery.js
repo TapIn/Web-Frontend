1ce91413719efaec8ed3fc80c0ee944ee8d2e959
Tyler Menezes <tylermenezes@gmail.com>
2012-07-25 18:35:46 -0700

    Improve flickering issue in slider

--- html/assets/js/tapin/frontend/videogallery.js
+++ html/assets/js/tapin/frontend/videogallery.js
@@ -1,22 +1,70 @@
-define(['jquery', 'tapin/util/log'], function(JQuery, Log) {
+define(['jquery', 'tapin/util/log', 'tapin/util', 'tapin/util/async'], function(JQuery, Log, Util, Async) {
     return function(elem)
     {
         var _this = this;
         var _elem = null;
 
-        this.addVideo = function(id, lambda)
+        var _ids = [];
+        this.commitUpdate = function()
         {
-            player.mute();
+            var groups = [];
+            var lastFocused = _elem.children('.item').index(_elem.children('.item .active'));
+            if (lastFocused < 0) {
+                lastFocused = 0;
+            }
+
+            for (var i in _ids) {
+                var groupID = Math.floor(i / 3);
+                if (typeof(groups[groupID]) === 'undefined') {
+                    groups[groupID] = [];
+                }
+                groups[groupID].push(_ids[i]);
+            }
+
+            var html = '';
+            for (var i in groups) {
+                var active = '';
+                if (i == lastFocused) {
+                    active = ' active';
+                }
+
+                var groupHtml = '<div class="item' + active + '">';
+                for (var j in groups[i]) {
+                    var url = 'http://thumbs.tapin.tv/' + groups[i][j] + '/144x107/latest.jpg?noCache=' + Util.randomString(30);
+                    var itemHtml = '<a href="#video/' + groups[i][j] + '/now"><img src="' + url + '" /></a>'
+                    groupHtml += itemHtml;
+                }
+                groupHtml += '</div>';
+
+                html += groupHtml;
+            }
+
+            var temp = _elem.clone();
+            _elem.before(temp);
+            temp.css('position', 'absolute');
+            temp.css('background', $('#sidebar').css('background'));
+            temp.offset(_elem.offset());
+
+            _elem.html(html);
+
+            // Give the new images time to load...
+            Async.later(1000, function(){
+                temp.remove();
+            });
+            _ids = [];
         }
 
-        this.removeVideo = function(id)
+        this.addVideo = function(id)
         {
-
+            _ids.push(id);
         }
 
         this.constructor = function(elem)
         {
-            _elem = elem;
+            elem.html('');
+            _elem = $('<div class="carousel-inner"></div>');
+            elem.append(_elem);
+            elem.append($('<a class="carousel-control left" href="#myCarousel" data-slide="prev"><p>‹</p></a><a class="carousel-control right" href="#myCarousel" data-slide="next"><p>›</p></a>'));
         }
         this.constructor(elem);
     }
