97164bae09bbc9a02a6487c51aa7f0df2ee92a67
Tyler Menezes <tylermenezes@gmail.com>
2012-10-02 09:05:49 +0000

    Remove old site

--- html/assets/js/mixpanel.js
+++
@@ -1,8 +0,0 @@
-/**
- * Mixpanel
- * Loads Mixpanel from Mixpanel's servers
- */
-define(['tapin/config', 'tapin/util/log'], function(Config, Log){
-    mixpanel.init(Config['mixpanel']['key']);
-    Log('debug', 'Initialized mixpanel with key ' + Config['mixpanel']['key']);
-})
